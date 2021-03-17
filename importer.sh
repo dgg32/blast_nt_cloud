f695f884a5c6b5153a0b28eb0b6e05e2711395cb
Sixing Huang <dgg32@users.noreply.github.com>
2021-03-17 12:24:07 +0100

    Update importer.sh

--- importer.sh
+++ importer.sh
@@ -3,7 +3,7 @@
 
 
 
-STR=$(find $1 -maxdepth 1 -regex '\S+.fasta' | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
+STR=$(find $1 -maxdepth 1 \( -name '*.fasta' -or -name "*.fa" \) | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
 
 
 #/home/sih13/tool/blast/blastp -db /home/sih13/database/md5nr/md5nr_no_hyphen -query $STR -num_threads 1 -max_target_seqs 10 -evalue 1e-10 -outfmt 6
