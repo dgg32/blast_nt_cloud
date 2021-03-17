4506ed5847375960ac4e3d01681c2a7eb0629dc1
Sixing Huang <dgg32@users.noreply.github.com>
2021-03-17 12:23:17 +0100

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -2,4 +2,4 @@
 
 #qsub -tc 30 -e /home/sih13/sge-log-files/ -o $1 -t 1-$(find $1 -maxdepth 1 -name "*.fasta_*"|wc -l) importer.sh $1
 
-sbatch -c 14 -p long --mem=128G --error $1/error.txt --output $1/output.txt --array=1-$(find $1 -maxdepth 1 -name "*.fasta"|wc -l)%30 importer.sh $1 $2
+sbatch -c 14 -p long --mem=128G --error $1/error.txt --output $1/output.txt --array=1-$(find $1 -maxdepth 1 \( -name "*.fasta" -or -name "*.fa" \) |wc -l)%30 importer.sh $1 $2
